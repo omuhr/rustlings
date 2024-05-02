e43884dec9d563709edc2415d6fd2a8163fa3ce9
muoscar <oscar.muhr@gmail.com>
2024-05-02 22:18:21 +0200

    progress

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_apples: usize) -> usize {
+    if num_apples > 40 {
+        return num_apples * 1;
+    }
+
+    return num_apples * 2;
+}
 
 // Don't modify this function!
 #[test]
